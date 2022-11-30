9e8870ebb4912a52078d262630575ada1bf4b1a8
tommy-l-ngo <tommyngo2@my.unt.edu>
2022-11-29 18:38:33 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_ZkcoobsmM',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '29df9f1i05diuocdp5fmj441k0',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
